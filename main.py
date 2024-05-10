b8387a2df1de7a26ba428fcb36688523e83953ba
spr593 <salome.perez8@gmail.com>
2024-05-10 14:40:16 -0500

    comparison between model's metrics

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from data_loader import *
 from preprocessing import *
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+def compare_models (ypred_m1, ypred_m2):
+    precision = precision_score(ypred_m1, ypred_m2, average="binary")
+    recall =  recall_score(ypred_m1, ypred_m2, average="binary")
+    f1 = f1_score(ypred_m1, ypred_m2, average="binary")
+    return precision, recall, f1
+
 
 if __name__=="__main":
     filepath = ""
@@ -11,3 +19,6 @@ if __name__=="__main":
 
 
     #Call agorithm 2 - SVM
+
+
+    #Compare models metrics
