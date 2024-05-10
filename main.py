c62a30b6f22cdedd495d136bbfb9e3e8410ac822
spr593 <salome.perez8@gmail.com>
2024-05-10 14:19:28 -0500

    update docs

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ if __name__=="__main":
     data_content = load_data(filepath)
     preproc_data = scale_features(data_content)
     preproc_lables = encode_labels(data_content)
+
+    #Call algorithm 1 - Logistic Regression
+
+
+    #Call agorithm 2 - SVM
